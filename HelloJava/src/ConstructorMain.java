ab595b2b55b8f93021331ee3e84417c78030f6c9
nidhijain <nidhi31jain@gmail.com>
2019-06-08 07:52:54 +0100

    added constructor code

---
+++ HelloJava/src/ConstructorMain.java
@@ -0,0 +1,11 @@
+public class ConstructorMain {
+    public static void main(String args[])
+    {
+
+        ConstuctorTest constuctorTest = new ConstuctorTest(5);
+         constuctorTest = new ConstuctorTest(2,"Hello");
+
+
+
+    }
+}
